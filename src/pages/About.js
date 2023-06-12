70e78cc78c40753b83c1cf8270ed0409e3225e7d
Henderson-Jake <henderson.jake.c@gmail.com>
2023-06-12 17:22:15 -0500

    Function Todo list! First time using HTML!

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import "./About.css";
+import jake_img from '../assets/jake_img.jpg';
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+    <div className="split left">
+      <div className="centered">
+        <img 
+          className="profile_image"
+          src = {jake_img}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="name_title">Jake Henderson</div>
+        <div className="brief_description">
+          <body>
+            <p>Hello! My name is Jake Henderson, I'm pursuing a Bachelor's Degree in Computer Science at the University of Texas at San Antonio! When I'm not tinkering on something 
+              tech related, you can find me training Muay Thai, reading, cooking, or spending time with friends and family!
+            </p>
+          </body>
+        </div>
+      </div>
+    </div>
+  </div> 
       </div>
     )
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import "./Home.css";
+import AddTodo from "../component/AddTodo.js";
+import Todos from "../component/todos.js";
 
 class Home extends Component {
   // A default state of this component with an empty list of todos.
@@ -6,6 +9,7 @@ class Home extends Component {
     super();
     this.state = {
       // create your empty list here call it todos.
+      todos : new Array()
     };
   }
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
@@ -27,8 +31,9 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <p> Replace this</p>
         <h1>Todo's </h1>
+        <AddTodo addTodo = {this.addTodo} />
+        <Todos todos = {this.state.todos} />
       </div>
     );
   }
